94e541a20832fc4d54940ff0d1223cb6d3a23c41
Florian Ferrier <Florian.Ferrier@smile.fr>
2020-03-22 16:42:56 +0100

    Start project with libopencm3 and a nucleo board (STM32F446RE)

---
+++ config/config.mk
@@ -0,0 +1,54 @@
+# MCU definitions
+MCU_TYPE=stm32
+MCU_FAMILLY=f4
+MCU_CORE=cortex-m4
+MCU_PRECISION=46re
+
+# Flash/debug tool configuration
+INTERFACE=stlink
+TRANSPORT=hla_swd
+TARGET=$(MCU_TYPE)$(MCU_FAMILLY)x
+
+# firmware name
+FIRMWARE=firmware.elf
+
+# build dir name
+BUILD_DIR=build
+RELEASE_DIR=$(BUILD_DIR)/release
+DEBUG_DIR=$(BUILD_DIR)/debug
+
+#  HAL (libopencm3)
+HAL=libopencm3
+
+# OS ("FreeRTOS" or "baremetal")
+OS=baremetal
+
+# FreeRTOS config
+# heap implementation
+FREERTOS_HEAP_IMPLEM=4
+# MCU port
+FREERTOS_MCU_PORT=GCC/ARM_CM3
+# config file
+FREERTOS_CONFIG=config/FreeRTOSConfig.h
+
+# linker script name
+LINK_FILE=$(LDSCRIPT)
+
+# openocd config file
+OPENOCD_CFG_FILE=config/openocd.cfg
+
+# cross compile prefix
+CROSS_COMPILE=arm-none-eabi-
+
+# compilation flags
+CC_OPT=-Os
+CFLAGS=-W -Wall
+CFLAGS_DEBUG=-g -gdwarf-4
+CC_DEFINES=
+
+# link flags
+LD_OPT=-flto
+LDFLAGS=-Wl,--gc-sections -nostartfiles -lnosys -Xlinker -Map=output.map
+
+# arch specific flags
+ARCH_FLAGS=-mcpu=$(MCU_CORE) -mthumb -msoft-float
---
+++ main.c
@@ -0,0 +1,69 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/cm3/systick.h>
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/exti.h>
+#include <libopencm3/stm32/usart.h>
+
+int main(void)
+{
+  rcc_periph_clock_enable(RCC_GPIOA);
+  gpio_mode_setup(GPIOA,
+                  GPIO_MODE_OUTPUT,
+                  GPIO_PUPD_NONE,
+                  GPIO5);
+  gpio_set_output_options(GPIOA,
+                          GPIO_OTYPE_PP,
+                          GPIO_OSPEED_2MHZ,
+                          GPIO5);
+  gpio_clear(GPIOA, GPIO5);
+
+  rcc_periph_clock_enable(RCC_GPIOC);
+  gpio_mode_setup(GPIOC,
+                  GPIO_MODE_INPUT,
+                  GPIO_PUPD_NONE,
+                  GPIO13);
+
+  rcc_periph_clock_enable(RCC_SYSCFG);
+  nvic_disable_irq(NVIC_EXTI15_10_IRQ);
+  exti_select_source(EXTI13, GPIOC);
+  exti_set_trigger(EXTI13, EXTI_TRIGGER_RISING);
+  exti_enable_request(EXTI13);
+  exti_reset_request(EXTI13);
+  nvic_set_priority(NVIC_EXTI15_10_IRQ, 255);
+  nvic_enable_irq(NVIC_EXTI15_10_IRQ);
+
+  rcc_periph_clock_enable(RCC_GPIOA);
+  gpio_mode_setup(GPIOA,
+                  GPIO_MODE_AF,
+                  GPIO_PUPD_NONE,
+                  GPIO2);
+  gpio_set_af(GPIOA, GPIO_AF7, GPIO2);
+
+  rcc_periph_clock_enable(RCC_USART2);
+	usart_set_baudrate(USART2, 115200);
+	usart_set_databits(USART2, 8);
+	usart_set_stopbits(USART2, USART_STOPBITS_1);
+	usart_set_mode(USART2, USART_MODE_TX);
+	usart_set_parity(USART2, USART_PARITY_NONE);
+	usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
+	usart_enable(USART2);
+
+  while(1);
+
+  return 0;
+}
+
+void exti15_10_isr(void)
+{
+  exti_reset_request(EXTI13);
+  gpio_toggle(GPIOA, GPIO5);
+  usart_send_blocking(USART2, 'A');
+  usart_send_blocking(USART2, '\r');
+  usart_send_blocking(USART2, '\n');
+}
