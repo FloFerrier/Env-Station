12c466a9f89d0bb0b2af16ca4d2d20df849b693a
Florian Ferrier <Florian.Ferrier@smile.fr>
2020-04-04 10:57:47 +0200

    Update main file for measuring light value (in Analog mode)

--- main.c
+++ main.c
@@ -11,6 +11,9 @@
 #include "BSP/button.h"
 #include "BSP/led.h"
 #include "BSP/uart.h"
+#include "BSP/adc.h"
+
+#include "Driver/GA1A1S202WP/ga1a1s202wp.h"
 
 #include "Debug/printf/printf.h"
 
@@ -18,6 +21,7 @@
 uint32_t SystemCoreClock;
 
 static TaskHandle_t xTask1 = NULL;
+static QueueHandle_t xQueue1 = NULL;
 
 static void vTask1(void *pvParameters);
 static void vTask2(void *pvParameters);
@@ -45,9 +49,11 @@ int main(void)
                             configMAX_PRIORITIES-1,
                             NULL);
 
+  xQueue1 = xQueueCreate(10, sizeof(uint32_t));
 
   if((task1_status == pdPASS) &&
-     (task2_status == pdPASS))
+     (task2_status == pdPASS) &&
+     (xQueue1 != NULL))
   {
     vTaskStartScheduler();
     taskENABLE_INTERRUPTS();
@@ -66,6 +72,19 @@ void exti15_10_isr(void)
   vTaskNotifyGiveFromISR(xTask1, &xHigherPriorityTaskWoken);
 }
 
+void adc_isr(void)
+{
+  BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+  uint32_t raw_value = 0;
+
+  if(adc_get_flag(ADC1, ADC_SR_EOC))
+  {
+    adc_clear_flag(ADC1, ADC_SR_EOC);
+    raw_value = adc_read_regular(ADC1);
+    xQueueSendFromISR(xQueue1, &raw_value,&xHigherPriorityTaskWoken);
+  }
+}
+
 void vTask1(void *pvParameters)
 {
   vLed_Setup();
@@ -82,11 +101,21 @@ void vTask1(void *pvParameters)
 void vTask2(void *pvParameters)
 {
   vUART_Setup();
+  vADC_Setup();
+
+  uint32_t raw_value = 0;
+  uint32_t volt_value = 0;
+  double lux_value = 0;
 
   printf("Debug Task 2\r\n");
 
   while(1)
   {
-    // Nothing for the moment
+    vTaskDelay(pdMS_TO_TICKS(2500));
+    adc_start_conversion_regular(ADC1);
+    xQueueReceive(xQueue1, &raw_value, portMAX_DELAY);
+    volt_value = xAdcRawToVolt(raw_value);
+    lux_value = xVoltToLux(volt_value);
+    printf("[LUX] Lux value : %.0lf\r\n", lux_value);
   }
 }
