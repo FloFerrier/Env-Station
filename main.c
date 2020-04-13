3f2fc535fa5ee09836d439e90cb494c9b38a5536
Florian Ferrier <Florian.Ferrier@smile.fr>
2020-04-13 15:57:26 +0200

    Update application for using HC05 bluetooth module

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include <libopencm3/cm3/systick.h>
 
@@ -11,13 +12,19 @@
 #include "BSP/button.h"
 #include "BSP/led.h"
 #include "BSP/uart.h"
+#include "BSP/gpio.h"
+
+#include "Driver/HC05/hc05.h"
 
 #include "Debug/printf/printf.h"
+
+#define MAX_BUFFER_UART_RX 255
 
 /* Necessary for FreeRTOS */
 uint32_t SystemCoreClock;
 
 static TaskHandle_t xTask1 = NULL;
+static QueueHandle_t xQueue2 = NULL;
 
 static void vTask1(void *pvParameters);
 static void vTask2(void *pvParameters);
@@ -42,12 +49,15 @@ int main(void)
                             "Task2",
                             configMINIMAL_STACK_SIZE,
                             NULL,
-                            configMAX_PRIORITIES-1,
+                            configMAX_PRIORITIES-2,
                             NULL);
+
+  xQueue2 = xQueueCreate(10, sizeof(char) * MAX_BUFFER_UART_RX);
 
 
   if((task1_status == pdPASS) &&
-     (task2_status == pdPASS))
+     (task2_status == pdPASS) &&
+     (xQueue2 != NULL))
   {
     vTaskStartScheduler();
     taskENABLE_INTERRUPTS();
@@ -66,10 +76,36 @@ void exti15_10_isr(void)
   vTaskNotifyGiveFromISR(xTask1, &xHigherPriorityTaskWoken);
 }
 
+/* ISR for buffering received data from UART3 */
+void usart3_isr(void)
+{
+  BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+  static volatile uint16_t c = 0;
+  static uint16_t i = 0;
+  static char buffer[MAX_BUFFER_UART_RX];
+
+  c = usart_recv(USART3);
+  buffer[i++] = (char)c;
+
+  if(c == '\n')
+  {
+    i = 0;
+    xQueueSendFromISR(xQueue2, buffer, &xHigherPriorityTaskWoken);
+  }
+  else if(i >= (MAX_BUFFER_UART_RX - 1))
+  {
+    i = 0;
+  }
+}
+
 void vTask1(void *pvParameters)
 {
+  (void) pvParameters;
   vLed_Setup();
   vPB_Setup();
+  vConsole_Setup();
+
+  printf("Debug LED\r\n");
 
   while(1)
   {
@@ -81,12 +117,46 @@ void vTask1(void *pvParameters)
 
 void vTask2(void *pvParameters)
 {
+  (void) pvParameters;
+
+  printf("Debug HC05\r\n");
+  int8_t rslt = HC05_OK;
+
+  vGPIO_Setup();
   vUART_Setup();
 
-  printf("Debug Task 2\r\n");
+  rslt = HC05_Setup("WS-V1", "0501");
 
-  while(1)
+  if(rslt != HC05_OK)
   {
-    // Nothing for the moment
+    printf("[HC05] Setup fail ...\r\n");
   }
+
+  printf("[HC05] Data Mode Start !\r\n");
+
+  /* Wait the receiver outside */
+  HC05_Cmp_Response("READY\r\n");
+
+  HC05_Send_Data("Hello World !\r\n");
+  if(HC05_Cmp_Response("ACK\r\n"))
+  {
+    printf("[HC05] ACK !\r\n");
+  }
+  else
+  {
+    printf("[HC05] No ACK ...\r\n");
+  }
+
+  vTaskDelete(NULL);
+}
+
+void HC05_Receive(char *p_str)
+{
+  xQueueReceive(xQueue2, p_str, portMAX_DELAY);
+  //printf("%s", p_str);
+}
+
+void HC05_Timer(uint32_t time)
+{
+  vTaskDelay(pdMS_TO_TICKS(time));
 }
