12272563a8b159ddc79800d74a050167ca3cf1af
flfer <florian.ferrier@aol.fr>
2021-12-26 14:37:32 +0100

    Increase value of spi clock

--- APPS/main.c
+++ APPS/main.c
@@ -244,7 +244,7 @@ void spi2_setup(void)
 {
   rcc_periph_clock_enable(RCC_SPI2);
   spi_disable(SPI2);
-  spi_init_master(SPI2, SPI_CR1_BAUDRATE_FPCLK_DIV_256,
+  spi_init_master(SPI2, SPI_CR1_BAUDRATE_FPCLK_DIV_2,
     SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE, SPI_CR1_CPHA_CLK_TRANSITION_1 , SPI_CR1_DFF_8BIT, SPI_CR1_MSBFIRST);
   spi_enable_ss_output(SPI2);
   spi_enable(SPI2);
